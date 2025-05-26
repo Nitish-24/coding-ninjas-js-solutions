84bfb5a792b6cc1200e6c26c14d576e184fbef6f
Nitish Rai <nrai24062002@gmail.com>
2025-05-26 23:17:39 +0530

    Adds function to convert concatenated strings to lowercase
    Introduces a new function that concatenates two input strings and converts the result to lowercase.
    
    This enhances string manipulation capabilities and ensures consistent casing for the output.

---
+++ convertToLowerCase.js
@@ -0,0 +1,9 @@
+lowerCaseString("Hello", "World");
+function lowerCaseString(str1, str2) {
+  let answer;
+  
+    let ans = str1.concat(str2);
+    answer = ans.toLowerCase();
+    console.log(answer);
+  return answer; 
+}
